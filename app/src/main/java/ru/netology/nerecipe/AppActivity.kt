dd719bf2f704e48d9d4874008fc7536c262b671e
Alexey Potapov <aloy@bk.ru>
2022-08-16 17:47:26 +0300

    User authentication implemented full work variant

--- app/src/main/java/ru/netology/nerecipe/AppActivity.kt
+++ app/src/main/java/ru/netology/nerecipe/AppActivity.kt
@@ -1,6 +1,38 @@
 package ru.netology.nerecipe
 
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class AppActivity : AppCompatActivity(R.layout.activity_app){
+class AppActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
+        val appBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        setSupportActionBar(appBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_logout -> {
+                Firebase.auth.signOut()
+                findNavController(R.id.feedFragment).navigate(R.id.welcomeFragment)
+                true
+            }
+            else -> false
+        }
+        return true
+    }
+
 }
